67773bd056fa5865f493316128e722a1095bc05c
aswanijehangeer <jahangiraswani237@gmail.com>
2024-05-13 02:49:08 +0500

    modules & packages/

--- Modules & Packages/modules-packages.py
+++ Modules & Packages/modules-packages.py
@@ -3,31 +3,31 @@
 
 
  
-# import func
-# from func import greeting, wish
-# from func import *
-# from func import greeting as greet, wish as wishing
+import func
+from func import greeting, wish
+from func import *
+from func import greeting as greet, wish as wishing
 
 
-# greeting("Jehangeer")
-# wish("Jehangeer")
+greeting("Jehangeer")
+wish("Jehangeer")
 
 
-# greet("Jehangeer")
-# wishing("Jehangeer")
+greet("Jehangeer")
+wishing("Jehangeer")
 
 
-## Package ----
+# Package ----
 
-# from func import func, func2
-# from func.func import wish
+from func import func, func2
+from func.func import wish
 
-# wish("Saraj")
+wish("Saraj")
 
-# func.greeting("Jehangeer")
-# func.wish("Jehangeer")
+func.greeting("Jehangeer")
+func.wish("Jehangeer")
 
-# func2.bye("Jehangeer")
+func2.bye("Jehangeer")
 
 
 
@@ -35,29 +35,29 @@
 
 from random import *
 
-# print(
-#     round(random(), 3),
-#     randint(0, 10),
-#     uniform(1.25, 2.0),
-#     randrange(0, 100, 4),
-#     randrange(1, 101, 2)
-# )
+print(
+    round(random(), 3),
+    randint(0, 10),
+    uniform(1.25, 2.0),
+    randrange(0, 100, 4),
+    randrange(1, 101, 2)
+)
 
-# def die(num):
-#     seed = num
-#     return randint(1, 6)
+def die(num):
+    seed = num
+    return randint(1, 6)
 
-# print(die(2))
+print(die(2))
 
-# lst = ["a", "b", "c", "d", "e", "f"]
-# shuffle(lst)
+lst = ["a", "b", "c", "d", "e", "f"]
+shuffle(lst)
 
-# print(lst)
+print(lst)
 
-# print(
-#     choice(lst),
-#     sample(lst, 3)
-# )
+print(
+    choice(lst),
+    sample(lst, 3)
+)
 
 
 ## Date-Time Module ----
